6143ed4458ac206c315f5c9dc32a62c330457b11
Gnatey <72100442+Gnatey@users.noreply.github.com>
2025-04-22 09:42:30 +0200

    push

--- code.py
+++ code.py
@@ -1,1 +1,7 @@
+import pandas as pd
+import streamlit as st
 
+url = 'https://raw.githubusercontent.com/Gnatey/M-moire_Deepfake/refs/heads/main/DeepFakes.csv'
+data = pd.read_csv(url)
+
+st.dataframe(data)
