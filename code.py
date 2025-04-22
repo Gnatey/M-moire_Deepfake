7a8ffe38d48d10fe24f8f55fa11e29e5393ea177
Gnatey <72100442+Gnatey@users.noreply.github.com>
2025-04-22 13:07:33 +0200

    push

--- code.py
+++ code.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 
 url = 'https://raw.githubusercontent.com/Gnatey/M-moire_Deepfake/refs/heads/main/DeepFakes.csv'
-data = pd.read_csv(url)
+data = pd.read_csv(url, delimiter=';') 
 
+st.title("Tableau de Bord DeepFakes")
 st.dataframe(data)
