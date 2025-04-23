47a6e944690cc67f8dc6d6fb008ab7ce95fb9d47
Gnatey <72100442+Gnatey@users.noreply.github.com>
2025-04-23 10:47:06 +0200

    a

--- app.py
+++ app.py
@@ -15,12 +15,6 @@ def load_data():
     url = 'https://raw.githubusercontent.com/Gnatey/M-moire_Deepfake/refs/heads/main/DeepFakes.csv'
     df = pd.read_csv(url, sep=';', encoding='utf-8')
 
-    # Nettoyage des noms de colonnes
-    df.columns = df.columns.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
-    df.columns = df.columns.str.strip().str.replace(' ', '_').str.lower()
-
-    df = df.dropna()
-
     return df
 
 df = load_data()
