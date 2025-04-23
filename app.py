d78fe6953788a01a0af164b5332b71c5eb01a82e
Gnatey <72100442+Gnatey@users.noreply.github.com>
2025-04-23 10:46:23 +0200

    a

--- app.py
+++ app.py
@@ -7,26 +7,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from PIL import Image
 
-@st.cache_data
-def load_data():
-    url = 'https://raw.githubusercontent.com/Gnatey/M-moire_Deepfake/refs/heads/main/DeepFakes.csv'
-    df = pd.read_csv(url, sep=';', encoding='utf-8')
-
-    # Nettoyage des noms de colonnes
-    df.columns = df.columns.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
-    df.columns = df.columns.str.strip().str.replace(' ', '_').str.lower()
-
-    df = df.dropna()  # Optionnel : enlève les lignes incomplètes
-
-    return df
-
-#-------------------------------------------------------------------------------------------#
-# ONGLET 1
-
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-
 # ================================
 # Chargement des Données
 # ================================
@@ -44,6 +24,9 @@ def load_data():
     return df
 
 df = load_data()
+
+#-------------------------------------------------------------------------------------------#
+# ONGLET 1
 
 # ================================
 # KPI
