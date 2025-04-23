cfb0356f6d470036189e29a4b554e4cefa5ef1a5
Gnatey <72100442+Gnatey@users.noreply.github.com>
2025-04-23 17:51:03 +0200

    a

--- app.py
+++ app.py
@@ -207,19 +207,19 @@ with tab2:
     elif chart_type == "Bar":
         fig_dynamic = px.bar(
         cross_data,
-        x='Count',
-        y=x_axis,
+        x=x_axis,
+        y='Count',
         color=color_by,
-        orientation='h',
         barmode='group',
-        title=f"📊 Bar Chart Horizontal : {x_axis} vs Count coloré par {color_by}",
-        facet_row=y_axis
+        title=f"📊 Bar Chart : {x_axis} vs Count coloré par {color_by}",
+        facet_col=y_axis
     )
         fig_dynamic.update_layout(
         height=800,
-        width=1000,
-        yaxis_title=x_axis,
-        xaxis_title="Count"
+        width=1200,
+        xaxis_tickangle=-45,  # Incline les labels
+        xaxis_title=x_axis,
+        yaxis_title="Count"
     )
 
     elif chart_type == "Treemap":
