245169236943d3d5e190e112932851b416b6d328
Gnatey <72100442+Gnatey@users.noreply.github.com>
2025-04-29 13:19:57 +0200

    a

--- app.py
+++ app.py
@@ -670,8 +670,8 @@ with st.expander("💬 Commentaires", expanded=False):
             col1, col2 = st.columns([1, 3])
             with col1:
                 user_name = st.text_input("Votre nom", max_chars=20, help="20 caractères max")
-            with col2:
-                user_comment = st.text_area("Votre commentaire", help="Partagez vos observations")
+        with col2:
+            user_comment = st.text_area("Votre commentaire", help="Partagez vos observations")
             
             submitted = st.form_submit_button("📤 Envoyer")
             
