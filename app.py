d5071e6530538549115f5c30df369f9eb13f27ca
Gnatey <72100442+Gnatey@users.noreply.github.com>
2025-05-10 16:46:02 +0200

    a

--- app.py
+++ app.py
@@ -623,8 +623,9 @@ def connect_to_gsheet():
 def load_users():
     sheet = connect_to_gsheet()
     data = sheet.get_all_records()
+    st.write("Contenu brut : ", data)  # ← ajout ici
     df = pd.DataFrame(data)
-    st.write("Colonnes utilisateurs chargées :", df.columns.tolist())  # debug
+    st.write("Colonnes utilisateurs chargées :", df.columns.tolist())
     return df
 
 def save_user(pseudo, password):
