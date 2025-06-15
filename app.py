2658393018e71a60bfdb80bdff58b666601e5254
Gnatey <72100442+Gnatey@users.noreply.github.com>
2025-06-15 14:30:45 +0200

    a

--- app.py
+++ app.py
@@ -1866,8 +1866,7 @@ with tab4:
     
     # Introduction méthodologique
     st.markdown("""
-    ### 🎯 Objectif : Nous appliquons une **méthodologie non supervisée** pour identifier automatiquement 
-    des groupes d'utilisateurs ayant des comportements similaires face aux DeepFakes.
+    ### 🎯 Objectif : Nous appliquons une **méthodologie non supervisée** pour identifier automatiquement des groupes d'utilisateurs ayant des comportements similaires face aux DeepFakes.
 
     """)
     
